70fa404d46e197b721d402201b12fcb110d3852d
Connor Shea <connor.james.shea@gmail.com>
2025-12-21 13:10:51 -0700

    Run formatter, add fmt config.

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,14 @@
-import React, { useMemo } from 'react';
+import React, { useMemo } from "react";
 
 export function Component() {
-  const foo = useMemo(() => (_unused: string, used: string) => {
-    return used;
-  }, []);
+  const foo = useMemo(
+    () => (_unused: string, used: string) => {
+      return used;
+    },
+    [],
+  );
 
-  const value = foo('ignore me', 'use me');
+  const value = foo("ignore me", "use me");
 
   return <div>Hello, world! {value}</div>;
 }
